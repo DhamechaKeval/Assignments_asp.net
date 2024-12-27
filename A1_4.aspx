1bdb17c8e83e9077d6a6c1b1801a76aa68fe3ff3
KevalDhamecha <kdhamecha541@rku.ac.in>
2024-12-27 16:55:04 +0530

    demonstrate “AutoPostBack” property

---
+++ A1_4.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="A1_4.aspx.cs" Inherits="Assignments_asp.net.A1_4" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="margin-left: 38px">
+            Keval Dhamecha
+            <br />
+            <br />
+            Create ASP.NET page to demonstrate “AutoPostBack” property for controls available.<br />
+            <br />
+            <h2> Select lable color</h2><asp:RadioButtonList ID="RadioButtonList1" runat="server" AutoPostBack="True" OnSelectedIndexChanged="RadioButtonList1_SelectedIndexChanged">
+                <asp:ListItem>Pink</asp:ListItem>
+                <asp:ListItem>Yellow</asp:ListItem>
+                <asp:ListItem>Green</asp:ListItem>
+            </asp:RadioButtonList>
+            <br />
+            &nbsp;<asp:Label ID="Label3" runat="server"></asp:Label>
+            <br />
+            <h3>select your city</h3>
+            <br />
+&nbsp;<asp:DropDownList ID="DropDownList1" runat="server" AutoPostBack="True" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged">
+                <asp:ListItem>Rajkot</asp:ListItem>
+                <asp:ListItem>Jamnagar</asp:ListItem>
+                <asp:ListItem>Surat</asp:ListItem>
+                <asp:ListItem>Porbandar</asp:ListItem>
+                <asp:ListItem>Junagadh</asp:ListItem>
+                <asp:ListItem>Morbi</asp:ListItem>
+                <asp:ListItem>Ahemdabad</asp:ListItem>
+            </asp:DropDownList>
+        &nbsp;&nbsp;
+            <br />
+            <br />
+&nbsp;<asp:Label ID="Label2" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ A1_4.aspx.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Assignments_asp.net
+{
+    public partial class A1_4 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void RadioButtonList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (RadioButtonList1.SelectedValue == "Pink")
+            {
+                Label3.Text = "You have selected Pink";
+                Label3.BackColor  = System.Drawing.Color.LightPink;
+                Label2.BackColor = System.Drawing.Color.LightPink;
+            }
+            else if (RadioButtonList1.SelectedValue == "Yellow")
+            {
+                Label3.Text = "You have selected Yellow";
+                Label3.BackColor = System.Drawing.Color.Yellow;
+                Label2.BackColor = System.Drawing.Color.Yellow;
+            }
+            else if (RadioButtonList1.SelectedValue == "Green")
+            {
+                Label3.Text = "You have selected Green";
+                Label3.BackColor = System.Drawing.Color.LightGreen;
+                Label2.BackColor = System.Drawing.Color.LightGreen;
+            }
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (DropDownList1.SelectedValue == "Rajkot")
+            {
+               Label2.Text = "You have selected Rajkot";
+            }
+            else if (DropDownList1.SelectedValue == "Jamnagar")
+            {
+                Label2.Text = "You have selected Jamnagar";
+            }
+            else if (DropDownList1.SelectedValue == "Surat")
+            {
+                Label2.Text = "You have selected Surat";
+            }
+            else if (DropDownList1.SelectedValue == "Ahemdabad")
+            {
+                Label2.Text = "You have selected Ahemdabad";
+            }
+            else if (DropDownList1.SelectedValue == "Porbandar")
+            {
+                Label2.Text = "You have selected Porbandar";
+            }
+            else if (DropDownList1.SelectedValue == "Morbi")
+            {
+                Label2.Text = "You have selected Morbi";
+            }
+            else if (DropDownList1.SelectedValue == "Ahemdabad")
+            {
+                Label2.Text = "You have selected Ahemdabad";
+            }
+        }
+    }
+}
